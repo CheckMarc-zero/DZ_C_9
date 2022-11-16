15fe4eb1a9b578b251bf7b79c182d9259e73efec
SandyAndy <ansigida77@gmail.com>
2022-11-16 18:58:44 +0300

    Задача 1.

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int N = insertNumber("Введите значение N");
+int a = 1;
+
+if (N<1) Console.WriteLine("Данные введены неправильно.");
+else 
+{
+    Console.WriteLine($"Числа от {N} до {a} :");
+    Console.WriteLine(RecFunc(a,N));
+}
+
+
+
+//Метод ввода данных
+int insertNumber (string message)
+{
+    Console.WriteLine(message);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+//Метод с рекурсией
+string RecFunc(int a, int N)
+{
+    if (a<=N) return RecFunc(a+1,N) + $"{a} ";
+    else return string.Empty;
+}
