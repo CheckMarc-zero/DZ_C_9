d17c7d51a63b54640b4b8792dd2b94290b135e4f
SandyAndy <ansigida77@gmail.com>
2022-11-16 20:46:19 +0300

    Задача 3.

---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int m = insertNumber("Введите m:");
+int n = insertNumber("Введите n:");
+
+
+if (n<0 || m<0) Console.WriteLine("Данные введены неправильно.");
+else 
+{
+    Console.WriteLine($"Значение функции Аккермана : {RecFunc(m,n)} ");
+}
+
+
+
+//Метод ввода данных
+int insertNumber (string message)
+{
+    Console.WriteLine(message);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+//Метод с рекурсией
+int RecFunc(int m, int n)
+{
+    if (m==0 && n>0) return n+1;
+    else if (m>0 && n==0) return  RecFunc(m-1,1);
+    else if (m>0 && n>0) return  RecFunc(m-1,RecFunc(m,n-1));
+    else return 0;
+}
